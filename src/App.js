aec31fbb8923ac742b5d992aff7e0f4129964c15
Anushka Joshi <anushkajoshi020503@gmail.com>
2025-01-28 23:41:14 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/hero" element={<Hero />} />
+        <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/blogs" element={<Blogs />} />
